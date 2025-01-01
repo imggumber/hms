560c468f63445d544619f9279cf7f851ac854d4a
Gurpreet Gumber <imgurpreetgumber@gmail.com>
2025-01-02 00:01:45 +0530

    Add copyright content

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,3 +1,3 @@
-<div class="footer">
-    this is footer area
+<div class="footer text-slate-500">
+    <p class="text-xs">&copy;  @php echo date('Y') . '-'  .( date('y') + 1) @endphp All Rights Reserved</p>
 </div>
