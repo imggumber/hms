5095d6dbd9c186f7afe4915ae0f6f885b7b6654f
Gurpreet Gumber <imgurpreetgumber@gmail.com>
2025-01-25 12:19:53 +0530

    Add department migration field

--- database/migrations/2025_01_24_114502_create_operations_table.php
+++ database/migrations/2025_01_24_114502_create_operations_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('operations', function (Blueprint $table) {
             $table->id();
+            $table->string('department');
             $table->timestamps();
         });
     }
--- database/migrations/2025_01_24_114542_create_patient_cares_table.php
+++ database/migrations/2025_01_24_114542_create_patient_cares_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('patient_cares', function (Blueprint $table) {
             $table->id();
+            $table->string('department');
             $table->timestamps();
         });
     }
